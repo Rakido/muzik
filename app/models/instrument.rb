a2a1b90d209058ce3bffbe1023b5441eb2b1ce2a
JJ Bernard <jjulien@kombolo.ooo>
2018-04-02 18:57:40 +0200

    setup models done

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,11 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+  has_many :reservations 
+
+  validates :city, presence: true
+  validates :category, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 600 } 
+  validates :price, presence: true
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :instrument
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
+  validates :status, presence: true, inclusion: { in: %w(pending accepted denied) }
 end

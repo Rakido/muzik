16ae164462a9d3a1e4f0b2b1b51e32c521c4be79
JJ Bernard <jjulien@kombolo.ooo>
2018-04-03 19:46:01 +0200

    Fix instruments controller

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,44 +1,36 @@
 class InstrumentsController < ApplicationController
-
-
   skip_before_action :authenticate_user!, only: [:home, :show]
-
-
-  def show
-    @instrument = Instrument.find(params[:id])
-  end
-
-
-
- def new
-   @instrument = Instrument.new
- end
-
- def create
-
-   @instrument = Instrument.new(instrument_params)
-   @instrument.user = current_user
-   @instrument.save
-
-   redirect_to profile_path
- end
-
- private
-
- def instrument_params
-   params.require(:instrument).permit(
-     :name,
-     :category,
-     :city,
-     :price,
-     :description,
-     :photo,
-     )
-   
-  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @instruments = Instrument.all
   end
 
+  def show
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
+    @instrument.save
+
+    redirect_to profile_path
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(
+      :name,
+      :category,
+      :city,
+      :price,
+      :description,
+      :photo
+    )   
+  end
 end

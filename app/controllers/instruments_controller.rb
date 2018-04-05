787b6008cc2d6bb0391d3b11f1d3fa635ec154d1
haydar taskin <tknhaydar@gmail.com>
2018-04-05 18:29:40 +0200

    updating-annonce
    delete annonce

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -41,6 +41,26 @@ class InstrumentsController < ApplicationController
     end
   end
 
+  def edit
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def update
+    @instrument = Instrument.find(params[:id])
+
+    if @instrument.update(instrument_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @instrument = Instrument.find(params[:id])
+    @instrument.destroy
+    redirect_to profile_path
+  end
+
   private
 
   def instrument_params

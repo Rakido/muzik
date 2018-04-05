9a5602ac886ac42b40fe3ea2a76c22c969cee3f8
JJ Bernard <jjulien@kombolo.ooo>
2018-04-05 15:22:53 +0200

    added filter and map to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @instruments = Instrument::CATEGORIES
+    @instruments = Instrument.all
     @last_instruments = Instrument.last(4)
   end
 
